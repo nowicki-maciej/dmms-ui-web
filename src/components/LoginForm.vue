7ed44ca977253021758e53e7be68a6ecfb56f7e1
Maciej Nowicki <nowicki.maaciej@gmail.com>
2019-03-15 23:34:35 +0100

    Added login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col md="4" offset-md="4" class="login-form">
+          <h1>Sign in to DMMS</h1>
+          <b-form @submit.prevent="onSubmit">
+            <b-form-group>
+              <b-form-input id="login-form_username"
+                            type="text"
+                            v-model="form.username"
+                            required
+                            placeholder="Username">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group>
+              <b-form-input id="login-form_password"
+                            type="password"
+                            v-model="form.password"
+                            required
+                            placeholder="Password">
+              </b-form-input>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Sign in</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: "LoginForm",
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+      }
+    },
+    methods: {
+      onSubmit: function () {
+        // const vm = this;
+        //
+        // let payload = {
+        //   email: this.form.email,
+        //   password: this.form.password
+        // };
+        //
+        // axios.post("http://localhost:9090/users", payload)
+        //   .then(function () {
+        //     vm.registerSuccessful = true;
+        //     vm.$router.push("/")
+        //   });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
