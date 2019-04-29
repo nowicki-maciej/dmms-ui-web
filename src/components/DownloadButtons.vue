dc35293bb5c87dfea9886eb16734a218af3a96ee
Maciej Nowicki <nowicki.maaciej@gmail.com>
2019-04-29 23:17:06 +0200

    Dmms 015 book details (#6)
    * Created PropDetailTable and BookDetails components
    
    * Finished book details functionality.
    
    * Added books download to buttons.
    
    * CR fixes

---
+++ src/components/DownloadButtons.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <template v-for="format in book.formats">
+      <a :href="'/api/books/' + book.id + '/' + format" download>
+        <b-button :class="formatClassName(format)">{{ format.toUpperCase() }}</b-button>
+      </a>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DownloadButtons",
+    props: ['book'],
+    methods: {
+      formatClassName: function (format) {
+        return 'format format-' + format.toLowerCase();
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .format {
+    color: black;
+    margin: 0 5px;
+  }
+
+  .format-pdf {
+    background-color: #ea9999;
+  }
+
+  .format-epub {
+    background-color: #f9cb9c;
+  }
+
+  .format-mobi {
+    background-color: #b4a7d6;
+  }
+</style>
