462331dd9baa8d5500c01896f93c222f8085802a
Maciej Nowicki <nowicki.maaciej@gmail.com>
2019-05-01 11:18:20 +0200

    Dmms 019 category management (#9)
    * Added basic functionality (no list yet)
    
    * Added category list

---
+++ src/components/category-management/CategoryManagement.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div id="container-btn-add-user">
+      <b-button id="btn-add-user" variant="success" v-b-modal.category-add-modal>
+        <font-awesome-icon icon="plus-square"/>
+        Add new category
+      </b-button>
+    </div>
+
+    <category-list :categories="categories" @change="refreshCategoriesList" />
+
+    <category-add-modal @change="refreshCategoriesList" />
+  </div>
+</template>
+
+<script>
+  import HttpClient from "../../helpers/HttpClient";
+  import CategoryAddModal from "./CategoryAddModal";
+  import CategoryList from "./CategoryList";
+
+  export default {
+    name: "CategoryManagement",
+    components: { CategoryList, CategoryAddModal },
+    mixins: [HttpClient],
+    data() {
+      return {
+        categories: [],
+      }
+    },
+    mounted: function () {
+      this.refreshCategoriesList();
+    },
+    methods: {
+      refreshCategoriesList: function () {
+        const vm = this;
+        this.$store.commit('appLoading', true);
+
+        this.get("/categories")
+          .then(response => {
+            vm.categories = response.data;
+          })
+          .finally(() => {
+            vm.$store.commit('appLoading', false);
+          });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #btn-add-user {
+    margin-bottom: 10px;
+    float: right;
+    clear: both;
+  }
+
+</style>
--- src/components/sidebar/menu/MenuList.vue
+++ src/components/sidebar/menu/MenuList.vue
@@ -2,6 +2,7 @@
   <ul>
     <menu-item icon="book" link="/library">Library</menu-item>
     <menu-item icon="users" link="/user-management">Users</menu-item>
+    <menu-item icon="archive" link="/category-management">Categories</menu-item>
     <menu-item icon="sign-out-alt" @click="logout">Logout</menu-item>
 <!--    <menu-item icon="share">Sharing</menu-item>-->
 <!--    <menu-item icon="cogs">Settings</menu-item>-->
