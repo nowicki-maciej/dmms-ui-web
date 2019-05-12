6ca17d3a570c3667f65a915c738801629c57b239
Maciej Nowicki <nowicki.maaciej@gmail.com>
2019-05-12 11:07:15 +0200

    Removed unused code

--- src/components/share/SharedToMe.vue
+++ src/components/share/SharedToMe.vue
@@ -13,28 +13,7 @@
     components: { SharedToMeList },
     data() {
       return {
-        books: [
-          // {
-          //   id: 1,
-          //   title: "Tytuł",
-          //   isbn: "123456",
-          //   description: "Opis książki",
-          //   authors: [{ name: "Jan", surname: "Kowalski", id: 1 }],
-          //   categories: [{ name: "Fantastyka", id: 4 }],
-          //   formats: ["PDF"],
-          //   owner: 'test'
-          // },
-          // {
-          //   id: 3,
-          //   title: "Tytuł 123",
-          //   isbn: "837563478",
-          //   description: "sdkjfnsdjf",
-          //   authors: [{ name: "Jan", surname: "Kowalski", id: 1 }],
-          //   categories: [{ name: "Historia", id: 7 }],
-          //   formats: ["MOBI", "PDF"],
-          //   owner: 'admin'
-          // },
-        ],
+        books: [],
       };
     },
     mounted() {
